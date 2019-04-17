e550566b053c9f76eba11fb08a5838b4b52deb90
takumin <takumiiinn@gmail.com>
2019-04-18 02:49:58 +0900

    vendor execute packer for bash function

--- build.bash
+++ build.bash
@@ -9,32 +9,47 @@ CURRENT_DIR="$(cd "$(dirname "$0")"; pwd)"
 # Vendor Directory
 VENDOR_DIR="${CURRENT_DIR}/vendor"
 
-# Check Packer Version
-if [ -z "${PACKER_VERSION}" ]; then
-	echo "Require Environment Variable: PACKER_VERSION"
-	exit 1
-fi
+# Packer Command
+PACKER_CMD="packer"
 
-# Create Vendor Directory
-if [ ! -d "${VENDOR_DIR}" ]; then
-	mkdir -p "${VENDOR_DIR}"
-fi
+# Vendor Execute
+vendor_execute() {
+	# Check Packer Version
+	if [ -z "${PACKER_VERSION}" ]; then
+		echo "Require Environment Variable: PACKER_VERSION"
+		exit 1
+	fi
 
-# Download Packer Archive
-if [ ! -f "${VENDOR_DIR}/packer_${PACKER_VERSION}_linux_amd64.zip" ]; then
-	wget -qO "${VENDOR_DIR}/packer_${PACKER_VERSION}_linux_amd64.zip" \
-		"https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_amd64.zip"
-fi
+	# Create Vendor Directory
+	if [ ! -d "${VENDOR_DIR}" ]; then
+		mkdir -p "${VENDOR_DIR}"
+	fi
 
-# Extract Packer Archive
-if [ ! -f "${VENDOR_DIR}/packer" ]; then
-	unzip -d "${VENDOR_DIR}" "${VENDOR_DIR}/packer_${PACKER_VERSION}_linux_amd64.zip"
-fi
+	# Download Packer Archive
+	if [ ! -f "${VENDOR_DIR}/packer_${PACKER_VERSION}_linux_amd64.zip" ]; then
+		wget -qO "${VENDOR_DIR}/packer_${PACKER_VERSION}_linux_amd64.zip" \
+			"https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_amd64.zip"
+	fi
 
-# Permission Packer Binary
-if [ ! -x "${VENDOR_DIR}/packer" ]; then
-	chmod +x "${VENDOR_DIR}/packer"
+	# Extract Packer Archive
+	if [ ! -f "${VENDOR_DIR}/packer" ]; then
+		unzip -d "${VENDOR_DIR}" "${VENDOR_DIR}/packer_${PACKER_VERSION}_linux_amd64.zip"
+	fi
+
+	# Permission Packer Binary
+	if [ ! -x "${VENDOR_DIR}/packer" ]; then
+		chmod +x "${VENDOR_DIR}/packer"
+	fi
+}
+
+# Check Installed Packer
+if ! type packer > /dev/null 2>&1; then
+	# Install Packer Command
+	vendor_execute
+
+	# Set Packer Command Variable
+	PACKER_CMD="${VENDOR_DIR}/packer"
 fi
 
 # Build Vagrant Box
-"${CURRENT_DIR}/vendor/packer" build "${CURRENT_DIR}/packer.json"
+"${PACKER_CMD}" build "${CURRENT_DIR}/packer.json"
